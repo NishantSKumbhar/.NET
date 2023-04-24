6978f085b7b9a410d791e16e5606c90d512cf72b
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-04-24 18:43:21 +0530

    Add project files.

---
+++ ConsoleApp1/Math/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Math
+{
+    public class Circle
+    {
+        public int radius;
+        public const float PI = 3.14f;
+        public float getAreaOfCircle()
+        {
+            return PI * (this.radius * this.radius);
+        }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,126 @@
+﻿using ConsoleApp1.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+{
+
+    public class Mobile
+    {
+        public string name;
+        public int price;
+        public int id;
+        public string color;
+        public Mobile()
+        {
+            Console.WriteLine("Mobile Default Constructor is called.");
+        }
+
+        public Mobile(string name, int price, int id, string color)
+        {
+            this.name = name;
+            this.price = price;
+            this.id = id;
+            this.color = color;
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"Id : {this.id}");
+            Console.WriteLine($"Name : {this.name}");
+            Console.WriteLine($"Price : {this.price}");
+            Console.WriteLine($"Color : {this.color}");
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(trailingZeros(25));
+            Console.WriteLine(gcd(78, 36));
+            Console.WriteLine(fastPower(3, 5));
+        }
+
+        
+
+
+        // FastPower
+        static int fastPower(int a, int b)
+        {
+            int res = 1;
+
+            while (b > 0)
+            {
+                
+                if ((b & 1) == 1)
+                {
+                    res = res * a;
+                }
+
+                a = a * a;
+                b = b / 2;
+            }
+
+            return res;
+
+        }
+
+
+        // GCD of a number
+        static int gcd(int num1, int num2)
+        {
+            if(num2 == 0)
+            {
+                return num1;
+            }
+
+            int ans = gcd(num2, num1 % num2);
+
+            return ans;
+        }
+
+
+        // Trailing zeros of a factorial of number;
+        static int trailingZeros(int number)
+        {
+            int ans = 0;
+
+            for(int i = 5; i <= number; i *= 5)
+            {
+                ans = ans + (number / i);
+            }
+
+            return ans;
+        }
+
+
+        static Boolean[] sieveOfEratosthenes(Boolean[] brr)
+        {
+            brr[0] = false; brr[1] = false;
+
+            for (int i = 2; i * i < brr.Length; i++)
+            {
+                for(int j = 2*i; j < brr.Length; j += 2 * i)
+                {
+                    brr[j] = false;
+                }
+            }
+
+            return brr;
+
+        }
+
+    }
+
+
+    
+}
